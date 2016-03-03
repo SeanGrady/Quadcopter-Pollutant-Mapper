6f081a8ebe0223d31f12393768186068250d2925
Sean Grady <vedicmonk@gmail.com>
2016-03-02 16:37:03 -0800

    synth_sensor working now, does not synch time yet

--- synth_sensor_gps.py
+++ synth_sensor_gps.py
@@ -7,19 +7,39 @@ class DataSynthesizer():
         self.parser = argparse.ArgumentParser()
         self.parser.add_argument('gps_file')
         self.parser.add_argument('sensor_file')
-        self.parser.add_arvument('-o', '--offset')
+        self.parser.add_argument('-o', '--offset')
         self.parser.parse_args(namespace=self)
         if not self.offset:
             self.offset = 0.
 
     def line_generator(self, filename):
         with open(filename, 'r') as f_obj:
-        for line in f_obj:
-            yield line
+            for line in f_obj:
+                yield line
+
+    def find_next_gps_line(self, gps_gen):
+        while True:
+            gps_reading = next(gps_gen)
+            gps_data = gps_reading.strip().split()
+            if gps_data[2] != "GPS_INT":
+                continue
+            else:
+                return gps_data
 
     def pair_readings(self):
         gps_gen = self.line_generator(self.gps_file)
         sensor_gen = self.line_generator(self.sensor_file)
         for line in sensor_gen:
             sensor_data = json.loads(line)
-            gps_reading = next(gps_gen)
+            sensor_time = sensor_data['ts']
+            gps_data = self.find_next_gps_line(gps_gen)
+            gps_time = gps_data[0]
+            lat = float(gps_data[6])
+            lon = float(gps_data[7])
+            lat = lat / 10000000
+            lon = lon / 10000000
+            print lat, lon, gps_time, sensor_time
+
+if __name__ == "__main__":
+    ds = DataSynthesizer()
+    ds.pair_readings()
